01cb555d4e71d93e471d1ffc80e20c8cc5029495
OsamaT-net <bakheetyaseen@gmail.com>
2026-04-26 13:48:27 -0400

    Initial commit

--- test.py
+++
@@ -1,9 +0,0 @@
-import cv2
-for i in range(3):
-    cap = cv2.VideoCapture(i)
-    ret, frame = cap.read()
-    if ret:
-        cv2.imshow(f"Camera {i}", frame)
-        cv2.waitKey(2000)  # shows for 2 seconds
-        cv2.destroyAllWindows()
-    cap.release()
